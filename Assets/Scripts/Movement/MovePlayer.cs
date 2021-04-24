8b60d1bbb46f3855657c494d3bbdab030ee70d0f
Rennie Keegan Wright <62271215+Rennie-K-Wright@users.noreply.github.com>
2021-04-23 20:27:46 -0700

    Added Horizontal movement and jumping

---
+++ Assets/Scripts/Movement/MovePlayer.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+
+    //Player variables
+    private Rigidbody2D player;
+    public float hSpeed = 1;
+    public float jumpForce = 5000;
+    private bool jumping;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getInput();
+        move();
+    }
+
+    private void getInput()
+    {
+        hSpeed = 0;
+        jumping = false;
+        // check for horizontal movement
+        if (Input.GetKeyDown(KeyCode.A)) hSpeed = -1;
+        else if (Input.GetKeyDown(KeyCode.D)) hSpeed = 1;
+
+        // check for jump
+        if (Input.GetKeyDown(KeyCode.Space)) jumping = true;
+    }
+
+    private void move()
+    {
+        // translate if horizontal movement exists
+        if (hSpeed != 0) transform.Translate(new Vector3(hSpeed, player.velocity.y, 0));
+        if (jumping) player.AddForce(new Vector2(0, jumpForce));
+        
+    }
+}
