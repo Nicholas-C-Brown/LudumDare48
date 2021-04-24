c2897d59d0a861efa759b0ec67675da255cf9b01
Rennie Keegan Wright <62271215+Rennie-K-Wright@users.noreply.github.com>
2021-04-23 20:54:07 -0700

    Update MovePlayer.cs
    Changed default jump force, refactored horizontal movement if statements, switched to editing player velocity in move()

--- Assets/Scripts/Movement/MovePlayer.cs
+++ Assets/Scripts/Movement/MovePlayer.cs
@@ -8,7 +8,7 @@ public class MovePlayer : MonoBehaviour
     //Player variables
     private Rigidbody2D player;
     public float hSpeed = 1;
-    public float jumpForce = 5000;
+    public float jumpForce = 490;
     private bool jumping;
     // Start is called before the first frame update
     void Start()
@@ -28,8 +28,14 @@ public class MovePlayer : MonoBehaviour
         hSpeed = 0;
         jumping = false;
         // check for horizontal movement
-        if (Input.GetKeyDown(KeyCode.A)) hSpeed = -1;
-        else if (Input.GetKeyDown(KeyCode.D)) hSpeed = 1;
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            hSpeed = -1;
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            hSpeed = 1;
+        }
 
         // check for jump
         if (Input.GetKeyDown(KeyCode.Space)) jumping = true;
@@ -38,7 +44,7 @@ public class MovePlayer : MonoBehaviour
     private void move()
     {
         // translate if horizontal movement exists
-        if (hSpeed != 0) transform.Translate(new Vector2(hSpeed, player.velocity.y));
+        player.velocity = new Vector2(player.velocity.x + hSpeed, player.velocity.y);
         if (jumping) player.AddForce(new Vector2(0, jumpForce));
         
     }
