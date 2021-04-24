ce8e1e83564cae5eeb66326083411572beb6d8b7
Rennie Keegan Wright <62271215+Rennie-K-Wright@users.noreply.github.com>
2021-04-23 21:30:47 -0700

    Update MovePlayer.cs
    Sprites now change between actions
    Sliding and Jumping animations appear along with running

--- Assets/Scripts/Movement/MovePlayer.cs
+++ Assets/Scripts/Movement/MovePlayer.cs
@@ -17,6 +17,9 @@ public class MovePlayer : MonoBehaviour
     [SerializeField]
     private float jumpForce = 400;
     private bool jumping;
+
+    [SerializeField]
+    private bool slide;
     
     void Start()
     {
@@ -34,6 +37,7 @@ public class MovePlayer : MonoBehaviour
     {
         moveDir = 0;
         jumping = false;
+        slide = false;
         // check for horizontal movement
         if (Input.GetKey(KeyCode.A))
         {
@@ -44,15 +48,36 @@ public class MovePlayer : MonoBehaviour
             moveDir = 1 * moveSpeed;
         }
 
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            slide = true;
+        }
+
         // check for jump
         if (Input.GetKeyDown(KeyCode.Space)) jumping = true;
     }
 
     private void Move()
     {
-        if (jumping) player.AddForce(Vector2.up * jumpForce);
-
+        if (jumping)
+        {
+            animator.SetBool("Jumping", true);
+            player.AddForce(Vector2.up * jumpForce);
+        } else
+        {
+            animator.SetBool("Jumping", false);
+        }
         player.velocity = new Vector2(moveDir, player.velocity.y);
+
+        if (slide)
+        {
+            animator.SetBool("Sliding", true);
+            Debug.Log("Yo we sliding");
+        } else
+        {
+            animator.SetBool("Sliding", false);
+        }
         
     }
+
 }
