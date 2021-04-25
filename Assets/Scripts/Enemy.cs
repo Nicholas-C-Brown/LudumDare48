343ee8fd23de5fd3ea0bc8ec34bffdd5629b1063
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2021-04-24 21:56:22 -0700

    new peices

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -17,10 +17,11 @@ public class Enemy : MonoBehaviour
         moving = true;
         controller.GameOverAction += () => moving = false;
     }
-
+    /*
     void Update()
     {
         if (moving) myRigidbody.velocity = new Vector2(Globals.MOVE_SPEED.x, myRigidbody.velocity.y);
         else myRigidbody.velocity = Vector2.zero;
     }
+    */
 }
